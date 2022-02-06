45ad78a99a60b4f14562089126fea998a1ca7453
Nico60 <ndhouailly@gmail.com>
2022-02-06 17:03:42 +0000

    NetworkTraffic: Show arrows in statusbar [1/2]
    arrow-edits:
    adapt code to our dynamic arrows method
    
    SKULSHADY: Reverse logic of the toggle
    spezi77: Adapt for statusbar traffic indicator
    
    Change-Id: Ie50dbe3f54495d4b5c0c0a51969df595de7a1386
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5487,6 +5487,12 @@ public final class Settings {
          */
         public static final String NETWORK_TRAFFIC_TYPE = "network_traffic_type";
 
+        /**
+         * Whether to disable showing arrows in statusbar network traffic indicators
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_ARROW = "network_traffic_arrow";
+
 	 /**
          * Use doubletap as doze pulse triggers
          * @hide
